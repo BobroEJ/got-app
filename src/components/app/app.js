3a17f385d1f10a123c62822c750613d5ef97f1ee
Evgeniy <tverdune@ya.ru>
2021-03-11 12:52:32 +0300

    098 Домашнее задание

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Col, Row, Container} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
@@ -6,29 +6,49 @@ import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+class App extends Component {
+
+    state = {
+        randomChar: true
+    }
+
+    toggleRandomChar = () => {
+            this.setState({
+                randomChar: !this.state.randomChar
+            })
+        }
+
+    render() {
+        
+
+        return (
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            {this.state.randomChar ? <RandomChar/> : null}
+                            <button type='button' 
+                                onClick={this.toggleRandomChar} 
+                                style={{margin:'0px auto 30px', padding:'10px', 'background-color':'#037', color: 'white',
+                                        'border-radius':'5px'}}
+                                >Toggle Random Char</button>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 };
 
 export default App;
